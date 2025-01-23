f839dd3d04ff3bf3ae40fc1f4960ba775974a93d
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-01-23 13:06:02 +0600

    create html template for php coding

--- index.php
+++ index.php
@@ -9,14 +9,21 @@
             width: 900px; 
             margin: 0 auto; 
             background: #ddd;
-            padding: 20px;
-            min-height: 400px;
         }
         .headeroption,
         .footeroption {
             background: #444;
             color: #fff;
             text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2,
+        .footeroption h2{
+            margin: 0;
+        }
+        .maincontent{
+            min-height: 500px;
+            padding: 20px;
         }
     </style>
 </head>
