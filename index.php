ac37212381ca18beeacbee09bb4ffc1846794e92
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-01-23 12:49:10 +0600

    php syntax

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Syntax</title>
+    <style>
+        .phpcoding{
+            width: 900px; 
+            margin: 0 auto; 
+            background: #ddd;
+            padding: 20px;
+            min-height: 400px;
+        }
+        .headeroption,
+        .footeroption {
+            background: #444;
+            color: #fff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    
+
+
+<div class="phpcoding">
+
+    <section class="headeroption">
+        <h2>PHP Fundamentals Training </h2>
+    </section>
+
+
+    <section class="maincontent">
+        <?php 
+            echo "I love Php";  
+        ?>
+    </section>
+
+
+    <section class="footeroption">
+        <h2>www.almumeetu.saiakt.com</h2>
+    </section>
+
+</div>
+
+
+
+</body>
+</html>
